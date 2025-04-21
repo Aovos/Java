be390ea8718ae9918cc354759fb735a0878d3e1f
Aovos <anastasious@gmx.net>
2025-04-21 21:36:16 +0200

    Method how to remove spaces in Strings

---
+++ Basics/Space_removing.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class Space_removing {
+
+        public static void main(String[] args) {
+
+            String name = "   Ann a   ";
+
+            // .trim() removes all spaces bevor and after
+            System.out.println(name.trim());
+
+            // removing all spaces including in between
+            System.out.println(name.replaceAll("\\s+", ""));
+        }
+
+}
